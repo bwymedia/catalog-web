fa3bf93d2a6d12b33c7048798dd984e55de97a78
Chase Stubblefield <cstubblefield@broadwaymedia.com>
2022-01-27 05:53:07 -0800

    Fix favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import PageLayout from '../components/PageLayout';
-import '../styles/globals.css';
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import PageLayout from "../components/PageLayout";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PageLayout>
       <Head>
         <title>Broadway Media Catalog</title>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </Head>
       <Component {...pageProps} />
     </PageLayout>
