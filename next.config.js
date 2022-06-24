42b6bf72769205617b32dd64b920f1eb3ed581b8
Robert Hogan <rhogan7@gmail.com>
2022-06-24 06:17:22 -0700

    Removed _domain path in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,38 +1,38 @@
 /** @type {import('next').NextConfig} */
-// module.exports = {
-//   reactStrictMode: true,
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/:path*",
-//         has: [
-//           {
-//             type: "host",
-//             value: process.env.CATALOG_DOMAIN,
-//           },
-//         ],
-//         destination: "/_domains/catalog/:path*",
-//       },
-//       {
-//         source: "/:path*",
-//         has: [
-//           {
-//             type: "host",
-//             value: process.env.DIGITALBACKDROPS_DOMAIN,
-//           },
-//         ],
-//         destination: "/_domains/digitalbackdrops/:path*",
-//       },
-//       {
-//         source: "/:path*",
-//         has: [
-//           {
-//             type: "host",
-//             value: process.env.SHOWS_DOMAIN,
-//           },
-//         ],
-//         destination: "/_domains/shows/:path*",
-//       },
-//     ];
-//   },
-// };
+module.exports = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: process.env.CATALOG_DOMAIN,
+          },
+        ],
+        destination: "/choreo/:path*",
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: process.env.DIGITALBACKDROPS_DOMAIN,
+          },
+        ],
+        destination: "/drops/:path*",
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: process.env.SHOWS_DOMAIN,
+          },
+        ],
+        destination: "/shows/:path*",
+      },
+    ];
+  },
+};
