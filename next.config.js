deb954d60e5d2538de70f60bf24aa435e5226d69
Robert Hogan <rhogan7@gmail.com>
2022-06-24 06:55:27 -0700

    Added environment variables to Vercel project catalog-web.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: process.env.CATALOG_DOMAIN,
+          },
+        ],
+        destination: "/choreo/:path*",
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: process.env.DIGITALBACKDROPS_DOMAIN,
+          },
+        ],
+        destination: "/:path*",
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: process.env.SHOWS_DOMAIN,
+          },
+        ],
+        destination: "/shows/:path*",
+      },
+    ];
+  },
 };
-
-module.exports = nextConfig;
