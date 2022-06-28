7e1672e573c94e056ed665c9743eea52022a6f6d
Robert Hogan <rhogan7@gmail.com>
2022-06-28 11:50:44 -0700

    Added local product set of first page.

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,22 +1,203 @@
 const product = [
   {
-    name: "Bananas",
-    description: "Yummy yellow fruit",
-    id: "sku_GBJ2Ep8246qeeT",
-    price: 400,
-    image:
-      "https://images.unsplash.com/photo-1574226516831-e1dff420e562?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=225&q=80",
-    attribution: "Photo by Priscilla Du Preez on Unsplash",
+    id: "13",
+    name: "50s Diner (Day, Exterior)",
+    price: "2.00",
     currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZ1BZIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--1bbde9323dec784fa70cea93b47760f9957f9400/278A-preview.webm",
   },
   {
-    name: "Tangerines",
-    id: "sku_GBJ2WWfMaGNC2Z",
-    price: 100,
-    image:
-      "https://images.unsplash.com/photo-1482012792084-a0c3725f289f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=225&q=80",
-    attribution: "Photo by Jonathan Pielmayer on Unsplash",
+    id: "64",
+    name: "Neon Signs - Warm",
+    price: "2.00",
     currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBai9ZIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--1f034bc975de651f552853a1ab8e1cc4fcd4bea0/400A-preview.webm",
+  },
+  {
+    id: "84",
+    name: "School Dance in the Auditorium (Evening, Interior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZzNkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--135e037ebeee45c66ccd61edff91ec50a7b3868f/061D-preview.webm",
+  },
+  {
+    id: "204",
+    name: "Fireworks in a Purple Sky (Night, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcDdaIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--d354da37f2db5c7c3f6ab60c40ef67d994d2464b/341A-preview.webm",
+  },
+  {
+    id: "296",
+    name: "Enchanted Forest (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdW5aIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--496d726bbb0a3f397a18722bc554db57ad5444ee/119A-preview.webm",
+  },
+  {
+    id: "448",
+    name: "Stormy Night at a Haunted Mansion (Night, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbzdhIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--97e96d6f8f16b118de647e6fbdc513c938aab688/497B-preview.webm",
+  },
+  {
+    id: "562",
+    name: "Fairytale Castle (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZ1RiIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--0b67a64e56f759d90e9f4227e2c7cac11c0b04c2/964A-preview.webm",
+  },
+  {
+    id: "568",
+    name: "Ice Castle with Purple Hue (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBanpiIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--a452a5c38f0a4b86e2dadcedc5840ae9ab1ab7ed/639B-preview.webm",
+  },
+  {
+    id: "1485",
+    name: "Trailer Park at Christmas (Night, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbDNiIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--d973b4fa89b4a0b1203088cf341e11b0ac626f4e/574B-preview.webm",
+  },
+  {
+    id: "3392",
+    name: "Island Oasis (Night, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbUhYIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--f5dae89306eaa42b1913651f6f42dfb0cf2c9aa9/A1S4_V.2_F_Loop-preview.webm",
+  },
+  {
+    id: "4097",
+    name: "Glitzy Stage with Yellow Spotlights (Interior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcHZkIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--d02e14bce33754ff1fd5639a3f1308f3a4ff8d72/390A-preview.webm",
+  },
+  {
+    id: "4163",
+    name: "Kaleidoscope Reds",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBc1BlIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--038e1c56635d1b3111aa7649d19332c14ebb85cf/135A-preview.webm",
+  },
+  {
+    id: "4184",
+    name: "Flowering European Village (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcHplIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--b6cb9bdae6e413a7d47baab850ffa39292a3ed66/197B-preview.webm",
+  },
+  {
+    id: "4192",
+    name: "Arabian Palace (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdGplIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--83bd2c2a53f199388764c425f4fd0b0f7249a870/216A-preview.webm",
+  },
+  {
+    id: "4193",
+    name: "Spooky Mansion on a Stormy Evening (Early Evening, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcmplIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--6dbebfb83f0008709d58e79af70d5aeaa0d68af1/220B-preview.webm",
+  },
+  {
+    id: "4231",
+    name: "Autumn Pathway (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBai9mIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--1e4ed99c9235f5120e645e2430df98b84ce2c388/322A-preview.webm",
+  },
+  {
+    id: "4256",
+    name: "Asian Garden (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb0RmIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--8ab4ee844402162b41bc503d80c4c4d76f86e8e2/449A-preview.webm",
+  },
+  {
+    id: "4267",
+    name: "Floating with Bubbles -Dim (Interior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcEhmIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--fe290f52d424ceecb869d6cf2c5a1b32b9168ef1/487_B-preview.webm",
+  },
+  {
+    id: "4381",
+    name: "North Pole Santa's Village (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdUxnIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--38227dac6f9ff8eef81c98ee38a360188039141b/632A-preview.webm",
+  },
+  {
+    id: "4394",
+    name: "Rosy Swirls",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBanZoIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--fece28c92cbb54c730053366729d6c633d347568/291B-preview.webm",
+  },
+  {
+    id: "4474",
+    name: "Snowy Waterfall (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbVhpIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--fe4732e0d0511e88814d9cd0f7c1132be3f14b3e/307A-preview.webm",
+  },
+  {
+    id: "4489",
+    name: "Yellow Brick Road (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBalhpIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--d397c2bb21b3572519742d549e20b1267325f5db/569A-preview.webm",
+  },
+  {
+    id: "4495",
+    name: "Chinese Theatre (Day, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBanJpIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--94dc5787bfe4993b6fb22057450d9541e45ecb2f/654A-preview.webm",
+  },
+  {
+    id: "4545",
+    name: "Blank Chalkboard (Day, Interior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBclBpIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--0d7d05005a4d9517078b984aae249bf0502a78e5/Chalkboard-Blank-preview.webm",
+  },
+  {
+    id: "4547",
+    name: "Mediterranean Beach (Sunrise, Exterior)",
+    price: "2.00",
+    currency: "USD",
+    previewVideo:
+      "https://accounts.broadwaymedia.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBc1hpIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2832893a9877438023ee53d100b12e9012214fc9/287B-preview.webm",
   },
 ];
 export default product;
